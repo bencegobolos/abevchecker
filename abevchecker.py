b50dca113c7ec08b82678bd55c1a0bc4299102df
Bence Gobolos <bencegobolos@gmail.com>
2017-06-18 13:58:20 +0200

    Update regexp in respect of 2017 changes

--- abevchecker.py
+++ abevchecker.py
@@ -5,34 +5,29 @@ regex_fejadat = re.compile("^VF\|(.)+$")
 regex_teteladat = re.compile("^VT\|(.)+$")
 regex_check_fejadat = re.compile(
     "^VF\|"
-    "12\|"
+    "13\|"
     "[a-zA-Z0-9]{,8}\|"
     "[a-zA-Z0-9]{13}\|"
     "(.){1,70}\|"
     "[a-zA-Z0-9]{,20}\|"
-    "[a-zA-Z0-9]{,10}\|"
-    "(.){3,20}\|"
+    "[a-zA-Z0-9]{,20}\|"
     "[0-9]{4}\|"
     "(.){3,20}\|"
     "(.){3,30}\|"
     "1\|"
-    "\|"
-    "\|"
-    "[a-zA-Z0-9]{2,20}\|"
+    "[a-zA-Z0-9]{,20}\|"
     "[0-9]{8}\|"
-    "[0-9]{8}"
+    "[0-9]{8}\|"
     "\s*$")
 regex_check_teteladat = re.compile(
     "^VT\|"
-    "[0-9a-zA-Z]{1,20}\|"
+    "[0-9a-zA-Z/]{1,20}\|"
     "[0-9]{8}\|"
     "(.){2,50}\|"
-    "([0-9]{1,12}\.[0-9]{2}|)\|"
+    "(-?[0-9]{1,12}\.[0-9]{2}|)\|"
     "[A-Za-z]{1,2}\|"
-    "([0-9]{1,4}\.[0-9]{2,3}|)\|"
+    "([0-9]{1,4}(\.[0-9]{2,3}){,1}|)\|"
     "([0-9]{1,3}\.[0-9]{2}|)\|"
-    "([0-9]{1,13}\.[0-9]{2}|)\|"
-    "HUF"
     "\s*$")
 
 
@@ -49,6 +44,19 @@ def is_fejadat_true(line):
 def is_teteladat_true(line):
     return regex_check_teteladat.match(line)
 
+"""
+def is_teteladat_pipe(line):
+	pipe_count = 0
+
+    for char in line:
+        if char == "|":
+            pipe_count += 1
+
+    if pipe_count != 9:
+        return False
+
+    return True
+"""
 
 def main():
     content = read_file(sys.argv[1])
